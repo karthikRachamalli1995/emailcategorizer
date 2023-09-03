0abba9b99647c8887fe07e2e19f7f96a7ae188d6
karthik <karthik.rachamalli1226@gmail.com>
2023-09-03 13:37:52 -0500

    Initial Commit

---
+++ src/main/java/com/projects/emailcategorizer/EmailcategorizerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.emailcategorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailcategorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailcategorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/emailcategorizer/EmailcategorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.emailcategorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailcategorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
